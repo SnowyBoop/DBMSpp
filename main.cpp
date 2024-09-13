3a71beb329ac6690d26f0a5acec470e3b2443f2c
fishsticks <150125982+gitsticks@users.noreply.github.com>
2024-09-13 21:32:16 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <cstdlib>
+#include <cstdio>
+#include <cstdio>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <array>
+
+std::string exec(const char* cmd) {
+    std::array<char, 128> buffer;
+    std::string result;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), static_cast<int>(buffer.size()), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+    return result;
+}
+
+int main() {
+  printf("hi\n");
+  std::string result = exec("wget https://erm.cool");
+  printf("done\n");
+  return 1;
+}
+
